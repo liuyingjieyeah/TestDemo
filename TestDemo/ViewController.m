4e06083868d9bffe311eb0b2fb42dfd373bff24d
怎样 <liuyingjieyeah@163.com>
2017-09-21 21:36:27 +0800

    Initial Commit

---
+++ TestDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestDemo
+//
+//  Created by 刘英杰 on 2017/9/21.
+//  Copyright © 2017年 刘英杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ TestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestDemo
+//
+//  Created by 刘英杰 on 2017/9/21.
+//  Copyright © 2017年 刘英杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
